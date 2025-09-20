fb7e7991a465e575d7bc1cb917465546b836a266
Shradha Pardeshi <135019132+ShradhaPardeshi1127@users.noreply.github.com>
2025-09-20 18:02:16 +0530

    Update Navbar.jsx

--- Interview-prep-application-dev/client/src/components/Navbar.jsx
+++ Interview-prep-application-dev/client/src/components/Navbar.jsx
@@ -23,9 +23,6 @@ const Navbar = (props) => {
                 <li className=" text-xl hover:scale-110 transition duration-300">
                     <Link to="/about">About</Link>
                 </li>
-                {/* <li className=" text-xl hover:scale-110 transition duration-300">
-                    <Link to="/contact">Contact</Link>
-                </li> */}
             </ul>
         </nav>
 
